b5d63bd5adc27b1cd27b017601b579de37037b76
heroku <87291560+wan5555@users.noreply.github.com>
2023-07-27 20:23:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'c9f8901b-d8f6-4e0e-a4b4-949ef59b01ab').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
